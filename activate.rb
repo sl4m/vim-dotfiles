79af0367d0ae73ef04fa69bab5ad01f22f4ceeb9
skim <skim.la@gmail.com>
2012-04-05 17:01:30 -0500

    adds tomorrow-theme, fugitive, command-t, vim-ruby submodules

--- activate.rb
+++ activate.rb
@@ -1,15 +1,24 @@
 #!/usr/bin/env ruby
+
 require 'fileutils'
 
 working_dir = File.expand_path(File.dirname(__FILE__))
 home_dir = File.expand_path("~")
-dot_files = Dir.glob(File.join(working_dir,"*"))
+dot_files = [File.join(working_dir, "vim"), File.join(working_dir, "vimrc")]
 
 dot_files.each do |filename|
-  next if filename =~ /activate\.rb$/
-
-  sym_link = File.join(home_dir,".#{File.basename(filename)}")
+  sym_link = File.join(home_dir, ".#{File.basename(filename)}")
 
   FileUtils.rm sym_link if File.symlink?(sym_link) || File.exist?(sym_link)
-  FileUtils.ln_s filename,sym_link
+  FileUtils.ln_s filename, sym_link
+end
+
+color_dir = File.join(working_dir, "vim", "colors")
+color_files = Dir.glob(File.join(color_dir, "colors", "*"))
+
+color_files.each do |filename|
+  sym_link = File.join(color_dir, File.basename(filename))
+
+  FileUtils.rm sym_link if File.symlink?(sym_link) || File.exist?(sym_link)
+  FileUtils.ln_s filename, sym_link
 end
