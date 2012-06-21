183d926355275f9447b6773590398c707f318b1a
skim <skim.la@gmail.com>
2012-06-20 23:23:44 -0500

    moves colors into bundle

--- activate.rb
+++ activate.rb
@@ -12,13 +12,3 @@ dot_files.each do |filename|
   FileUtils.rm sym_link if File.symlink?(sym_link) || File.exist?(sym_link)
   FileUtils.ln_s filename, sym_link
 end
-
-color_dir = File.join(working_dir, "vim", "colors")
-color_files = Dir.glob(File.join(color_dir, "colors", "*"))
-
-color_files.each do |filename|
-  sym_link = File.join(color_dir, File.basename(filename))
-
-  FileUtils.rm sym_link if File.symlink?(sym_link) || File.exist?(sym_link)
-  FileUtils.ln_s filename, sym_link
-end
